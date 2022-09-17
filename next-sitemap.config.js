facdaf6401cba48caf60b3f43097cf35d438db9f
radespratama <puturades89@gmail.com>
2022-09-18 07:12:57 +0800

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://example.com",
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://hey.radespratama.com",
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
 };

a674dc89554812a7b0854e54912b274097b62638
radespratama <puturades89@gmail.com>
2022-09-18 00:15:00 +0800

    add analytics

--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -1,1 +1,2 @@
 export * from "./contentData";
+export * from "./gTag";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,49 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
 import NextNProgress from "nextjs-progressbar";
+
+import * as gtag from "@/libs/utils";
 
 import "@/styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <NextNProgress
         color="#77ACF1"
         startPosition={0.3}

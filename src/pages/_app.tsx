c7c82391c04655e8deab8c41f8d1d07ba4d1f376
radespratama <puturades89@gmail.com>
2022-09-21 22:12:35 +0800

    change strategy Script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,12 +27,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
           window.dataLayer = window.dataLayer || [];
